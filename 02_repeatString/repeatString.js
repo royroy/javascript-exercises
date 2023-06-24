53982fd50aeea7792313ff9927e046326d0d5508
royroy <rayramjattan@gmail.com>
2023-06-24 07:28:49 -0400

    finish ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,11 @@
 const repeatString = function(word, times) {
-if (times <0) return "ERROR";
-let string = "";
-for (let i = 0; i<times; i++);{
+    if (times <0) return "ERROR";
+    let string = "";
+    for (let i = 0; i<times; i++){
     string += word;
 }
 return string;
 };
-repeatString("hey",7);
+
 // Do not edit below this line
 module.exports = repeatString;
